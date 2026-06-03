076cfc6f4d4ab7b3e79f3354783c64a1ba511199
Negin Mashayekhi <59525905+ngnma@users.noreply.github.com>
2026-06-03 11:47:13 +0100

    Add database connection and SQL query function

---
+++ db.py
@@ -0,0 +1,16 @@
+import requests
+import oracledb
+import pandas as pd
+
+connection = oracledb.connect(
+    user="ADMIN",
+    password="YOUR_DATABASE_PASSWORD",
+    dsn="YOURDB_low",
+    config_dir="/home/datascience/wallet",
+    wallet_location="/home/datascience/wallet",
+    wallet_password="YOUR_WALLET_PASSWORD"
+)
+
+def run_sql(query="SELECT * FROM AMAZON FETCH FIRST 10 ROWS ONLY")
+    df = pd.read_sql(query, connection)
+    print(df.head())
